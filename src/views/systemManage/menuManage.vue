322d617965dacd843d969c4261705fa359e8aa35
taohang <xiaotaokh@163.com>
2019-10-15 09:49:11 +0800

    菜单管理

--- src/views/systemManage/menuManage.vue
+++ src/views/systemManage/menuManage.vue
@@ -18,9 +18,11 @@
           default-expand-all
           :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
         >
-          <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
-          <el-table-column prop="name" label="姓名" sortable width="180"></el-table-column>
-          <el-table-column prop="address" label="地址"></el-table-column>
+          <el-table-column prop="date" label="菜单 / 按钮名称" width="180"></el-table-column>
+          <el-table-column prop="name" label="图标" width="80"></el-table-column>
+          <el-table-column prop="name" label="路由" width="280"></el-table-column>
+          <el-table-column prop="name" label="类型" width="120"></el-table-column>
+          <el-table-column prop="address" label="操作"></el-table-column>
         </el-table>
       </el-col>
     </el-row>
@@ -33,42 +35,7 @@ export default {
   watch: {},
   data() {
     return {
-      tableData: [
-        {
-          id: 1,
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-          children: [
-            {
-              id: 11,
-              date: "2016-05-01",
-              name: "王小虎",
-              address: "上海市普陀区金沙江路 1519 弄",
-              children: [
-                {
-                  id: 111,
-                  date: "2016-05-01",
-                  name: "王小虎",
-                  address: "上海市普陀区金沙江路 1519 弄"
-                },
-                {
-                  id: 112,
-                  date: "2016-05-01",
-                  name: "王小虎",
-                  address: "上海市普陀区金沙江路 1519 弄"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          id: 2,
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      tableData: [],
     };
   },
   methods: {},
